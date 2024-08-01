72b0589bd98cb812e69ff432ec36d707e516f429
Hema chowdhary <hemamadhala7@gmail.com>
2024-08-01 21:15:53 +0530

    Start of sricreations

---
+++ SriCreations/src/test/java/com/app/SriCreations/SriCreationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SriCreations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriCreationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
